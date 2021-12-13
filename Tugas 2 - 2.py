baec3be76cb6808da077e6bbed2132f61897d2d5
Zaniiiii <96063759+Zaniiiii@users.noreply.github.com>
2021-12-13 22:19:33 +0700

    Delete Tugas 2 - 2.py

--- Tugas 2 - 2.py
+++
@@ -1,9 +0,0 @@
-def indeks(angka):
-    target = angka[-1]
-    for i in angka:
-        for j in angka:
-            if int(i) + int(j) == int(target):
-                return [angka.index(i),angka.index(j)]
-
-n = input().split()
-print (indeks(n))
